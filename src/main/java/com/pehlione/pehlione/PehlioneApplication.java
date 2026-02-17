7e9611e15f543d652a9b1a21a3d14f5c65b82492
Mustafa Özdemir <mustafa.ozdemir1408@gmail.com>
2026-02-17 15:18:06 +0100

    first commit

---
+++ src/main/java/com/pehlione/pehlione/PehlioneApplication.java
@@ -0,0 +1,13 @@
+package com.pehlione.pehlione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PehlioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PehlioneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pehlione/pehlione/PehlioneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pehlione.pehlione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PehlioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
